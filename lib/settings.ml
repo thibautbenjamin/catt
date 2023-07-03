974307829dedf2213d1a4a73a755af80a51ab11d
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2023-07-03 16:46:12 +0200

    add setting for pretty printing (but no pretty printing yet)

--- lib/settings.ml
+++ lib/settings.ml
@@ -1,2 +1,3 @@
 let explicit_substitutions = ref false
 let verbosity = ref 0
+let pretty_printing = ref true
--- lib/settings.mli
+++ lib/settings.mli
@@ -1,2 +1,3 @@
 val explicit_substitutions : bool ref
 val verbosity : int ref
+val pretty_printing : bool ref
