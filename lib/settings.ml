20d54e6390eda89531ad040adbbf79619da24d67
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2023-07-05 12:33:08 +0200

    add (unused) setting for implicit suspensions

--- lib/settings.ml
+++ lib/settings.ml
@@ -1,3 +1,4 @@
 let explicit_substitutions = ref false
 let verbosity = ref 0
 let pretty_printing = ref true
+let implicit_suspension = ref true
--- lib/settings.mli
+++ lib/settings.mli
@@ -1,3 +1,4 @@
 val explicit_substitutions : bool ref
 val verbosity : int ref
 val pretty_printing : bool ref
+val implicit_suspension : bool ref
