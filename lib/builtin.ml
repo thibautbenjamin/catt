3365d291be099309fe6e392c0ccdc472c471801c
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2023-08-11 15:00:23 +0100

    [builtins] more consistent naming and api improvement

--- lib/builtin.ml
+++ lib/builtin.ml
@@ -16,26 +16,29 @@ module Memo = struct
     check_coh (Br[]) (Arr(Obj,Var(Db 0),Var(Db 0))) ("builtin_id", 0, None)
 end
 
-let comp_arity s =
+let arity_comp s =
   let n = List.length s in
   if !Settings.explicit_substitutions then
     (n-1)/2
   else n
 
-let rec comp_ps i =
+let rec ps_comp i =
   match i with
   | i when i <= 0 -> Error.fatal "builtin composition with less than 0 argument"
   | i when i = 1 -> Br [Br[]]
   | i ->
-    match comp_ps (i-1) with
+    match ps_comp (i-1) with
     | Br l -> Br (Br[] :: l)
 
-let comp s =
-  let arity = comp_arity s in
+let comp_n arity =
   let build_comp i =
-    let ps = comp_ps i in
+    let ps = ps_comp i in
     Coh.check_noninv ps (Var (Db 0)) (Var(Db 0)) ("builtin_comp", 0, None)
   in
   Memo.find arity build_comp
+
+let comp s =
+  let arity = arity_comp s in
+  comp_n arity
 
 let id = Memo.id
--- lib/builtin.mli
+++ lib/builtin.mli
@@ -1,5 +1,9 @@
 open Raw_types
+open Common
 open Kernel
+open Unchecked_types.Unchecked_types(Coh)
 
+val ps_comp : int -> ps
+val comp_n : int -> Coh.t
 val comp : subR -> Coh.t
 val id : Coh.t
