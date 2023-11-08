ef4779edbab633b68153759254fa9ba50440dd02
regular-citizen <chiara@sarti.me>
2023-11-08 16:04:15 +0000

    Fixed suspension problem, exposed codim 1 functorialisation to syntax.

--- lib/builtin.mli
+++ lib/builtin.mli
@@ -3,7 +3,6 @@ open Common
 open Kernel
 open Unchecked_types.Unchecked_types(Coh)
 
-val func_codim1_fn : (Coh.t -> Var.t list -> Coh.t) ref
 val ps_comp : int -> ps
 val comp_n : int -> Coh.t
 val comp : subR -> Coh.t
