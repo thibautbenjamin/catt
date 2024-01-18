943e9351d49c73f0810f3a51c8a33b845f3a89bc
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2024-01-18 15:33:08 +0000

    [inverses] test for applied invertible coherences

--- lib/inverse.ml
+++ lib/inverse.ml
@@ -2,21 +2,25 @@ open Kernel
 open Unchecked_types.Unchecked_types(Coh)
 
 exception NotInvertible of string
+exception CohNonInv
+
+let coh c =
+  let ps,ty,(name,susp,func) = Coh.forget c in
+  if (not (Coh.is_inv c)) then raise CohNonInv;
+  let ty_inv = match ty with
+    | Obj | Meta_ty _ -> assert false
+    | Arr (a,u,v) -> Arr (a,v,u)
+  in
+  check_coh ps ty_inv (name^"^-1",susp,func)
 
 let compute_inverse t =
   match t with
   | Var x -> raise (NotInvertible (Var.to_string x))
   | Meta_tm _ -> t (* Not sure about this case *)
-  | Coh(coh,sub) ->
-    let ps,ty,(name,susp,func) = Coh.forget coh in
-    if Coh.is_inv coh then
-      let ty_inv = match ty with
-        | Obj | Meta_ty _ -> assert false
-        | Arr (a,u,v) -> Arr (a,v,u)
-      in
-      let coh_inv = check_coh ps ty_inv (name^"^-1",susp,func)  in
-      Coh(coh_inv, sub)
-    else assert false
+  | Coh(c,sub) ->
+     try
+      let c_inv = coh c in Coh (c_inv, sub)
+    with CohNonInv -> assert false
 
 let compute_inverse t =
   try compute_inverse t with
