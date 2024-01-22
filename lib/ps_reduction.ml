b9580d01c4f86041cd292728f293bd42e59c9795
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2024-01-22 13:19:04 +0000

    [ps-reduction] definition of the reduction

---
+++ lib/ps_reduction.ml
@@ -0,0 +1,39 @@
+open Common
+open Kernel
+open Unchecked_types.Unchecked_types(Coh)
+
+let tdb i = Var (Var.Db i)
+
+let rec reduce i ps =
+  match i,ps with
+  | _, Br [] -> Br []
+  | 0, _ -> Br [Br []]
+  | i, Br l -> Br (List.map (reduce (i-1)) l)
+
+let reduction_sub ps =
+  let rec aux i ps =
+    match i,ps with
+    | _, Br[] -> [tdb 0, true]
+    | 0, Br l ->
+      let k = List.length l in
+      [Coh(Builtin.comp_n k,(Unchecked.(identity_ps (Br l)))), true ;
+        tdb (2*k-1), false;
+        tdb 0, true]
+    | i, Br l -> Unchecked.suspwedge_subs_ps (List.map (aux (i-1)) l) l
+  in
+  aux (Unchecked.dim_ps ps - 1) ps
+
+let coh c =
+  let ps,ty,name = Coh.forget c in
+  let name = Unchecked.full_name name in
+  let ps = reduce (Unchecked.dim_ps ps - 1) ps in
+  if Coh.is_inv c then
+    let src,tgt =
+      match ty with
+      | Arr (_,src,tgt) -> src,tgt
+      | _ -> Error.fatal "type of a coherence must be an arrow type"
+    in
+    Coh.check_inv ps src tgt (name^"_red", 0, None)
+  else
+    let src,tgt = Coh.noninv_srctgt c in
+    Coh.check_noninv ps src tgt (name^"_red", 0, None)
