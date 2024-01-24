27d74e59db3e0106c1cb59349ccb46c09bd9db5d
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2024-01-24 12:36:37 +0000

    [functorialisation] define functorialisation wrt all arguments

--- lib/functorialisation.mli
+++ lib/functorialisation.mli
@@ -3,4 +3,5 @@ open Kernel
 open Unchecked_types.Unchecked_types(Coh)
 
 val coh : Coh.t -> functorialisation_data -> Coh.t
+val coh_all : Coh.t -> Coh.t
 val tm : ctx -> tm -> functorialisation_data -> ctx * tm
