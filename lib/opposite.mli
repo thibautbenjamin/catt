b3805847cde857075fa782e534eb6af915f05fea
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2024-01-31 10:43:47 +0000

    [opposites] definition of the opposites

---
+++ lib/opposite.mli
@@ -0,0 +1,10 @@
+open Common
+open Kernel
+open Unchecked_types.Unchecked_types(Coh)
+
+type op_data = int list
+
+val op_data_to_string : op_data -> string
+val equiv_op_ps : ps -> op_data -> sub_ps
+val tm : tm -> op_data -> tm
+val coh : Coh.t -> op_data -> Coh.t
