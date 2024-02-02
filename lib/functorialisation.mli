562ceaaf5c639cffd77ada54218b0f370f49392b
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2024-02-02 10:50:03 +0000

    [to fix] recursive algorithm

--- lib/functorialisation.mli
+++ lib/functorialisation.mli
@@ -5,6 +5,3 @@ open Unchecked_types.Unchecked_types(Coh)
 val coh : Coh.t -> functorialisation_data -> Coh.t
 val coh_all : Coh.t -> Coh.t
 val tm : ctx -> tm -> functorialisation_data -> ctx * tm
-
-(* Temp exposing *)
-val coh_one_step_codim1 : Coh.t -> Var.t list -> Coh.t
