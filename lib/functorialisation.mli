1a125d0ae5cc0b4f593b0a427017dc56ed83d8f9
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2024-02-22 15:27:43 +0000

    fix rebase issue :)

--- lib/functorialisation.mli
+++ lib/functorialisation.mli
@@ -2,6 +2,8 @@ open Common
 open Kernel
 open Unchecked_types.Unchecked_types(Coh)
 
+val builtin_comp : (int -> Coh.t) ref
+
 val coh : Coh.t -> functorialisation_data -> Coh.t
 val coh_all : Coh.t -> Coh.t
 val tm : ctx -> tm -> functorialisation_data -> ctx * tm
