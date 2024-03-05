273cf812dba765e4202dea77a1c55c5993bc5693
regular-citizen <chiara@sarti.me>
2024-03-05 11:48:57 +0000

    implemented generalized functorialisation of types

--- lib/functorialisation.mli
+++ lib/functorialisation.mli
@@ -3,6 +3,7 @@ open Kernel
 open Unchecked_types.Unchecked_types(Coh)
 
 val builtin_comp : (int -> Coh.t) ref
+val builtin_whisk : (int -> int -> int -> Coh.t) ref
 
 val coh : Coh.t -> functorialisation_data -> Coh.t
 val coh_all : Coh.t -> Coh.t
