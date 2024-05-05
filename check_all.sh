ed1ef10b0483ce3906c854bf869a0a7ba75c7135
regular-citizen <chiara@sarti.me>
2024-05-05 21:43:17 +0100

    fixed tests, added script to run all

---
+++ check_all.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+TESTS=(
+"test/bug.catt"
+"test/builtin-comp.catt"
+"test/functorialisation.catt"
+"test/functoriality.catt"
+"test/implicit_subs.catt"
+"test/inverses.catt"
+"test/issue7.catt"
+"test/naturality.catt"
+"test/opposites.catt"
+"test/pretty-print.catt"
+"test/ps-syntax.catt"
+"test/suspension.catt"
+"test/test.catt"
+"test/vanilla.catt"
+"test/wildcards.catt"
+)
+
+for file in ${TESTS[@]}; do
+  echo "Running: $file"
+  dune exec -- catt "$file" || exit 1
+done
+
