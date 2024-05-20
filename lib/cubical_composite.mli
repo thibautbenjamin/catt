8ea2ac5e3f09e93cab5cae7943d534c179479d99
regular-citizen <chiara@sarti.me>
2024-05-20 14:12:06 +0100

    moved depth-1 interchanger code out of functorialisation

--- lib/cubical_composite.mli
+++ lib/cubical_composite.mli
@@ -1,9 +1,7 @@
-open Raw_types
 open Kernel
 open Unchecked_types.Unchecked_types(Coh)
 
 val comp_kn_tm : int -> int -> tm
 val comp_n_tm : int -> tm
-val ccomp : subR -> tm
 
 val init : unit ->  unit
