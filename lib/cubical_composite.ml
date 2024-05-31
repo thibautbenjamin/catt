40d0c3d5917370059cf8856d46d38fd461658978
regular-citizen <chiara@sarti.me>
2024-05-31 18:03:39 +0100

    cubical composites, inlined opposites, added memoization

--- lib/cubical_composite.ml
+++ lib/cubical_composite.ml
@@ -1,16 +1,23 @@
 open Common
 open Kernel
 open Unchecked_types.Unchecked_types(Coh)
-(*
-https://q.uiver.app/#q=WzAsNCxbMCwwLCIwIl0sWzIsMCwiMyJdLFsyLDIsIjQiXSxbMCwyLCIxIl0sWzEsMiwiNSIsMV0sWzAsMywiMiIsMV0sWzMsMSwiOCIsMSx7InNob3J0ZW4iOnsic291cmNlIjoyMCwidGFyZ2V0IjoyMH0sImxldmVsIjoyfV0sWzAsMSwiNiIsMV0sWzMsMiwiNyIsMV0sWzMsMiwiNyIsMSx7ImN1cnZlIjozfV0sWzAsMSwiNiIsMSx7ImN1cnZlIjotM31dLFs5LDgsIiIsMSx7InNob3J0ZW4iOnsic291cmNlIjoyMCwidGFyZ2V0IjoyMH19XSxbNywxMCwiIiwxLHsic2hvcnRlbiI6eyJzb3VyY2UiOjIwLCJ0YXJnZXQiOjIwfX1dXQ==
-*)
+
+module Memo = struct
+  let tbl_ccomp = Hashtbl.create 24
+
+  let find_ccomp i f =
+    try Hashtbl.find tbl_ccomp i with
+    | Not_found ->
+      let res = f i in
+      Hashtbl.add tbl_ccomp i res;
+      res
+end
 
 let tdb i = Var (Db i)
 let comp_n_l n = if n = 0 then tdb 0 else tdb ((2*n)-1)
 let comp_n_r n = tdb ((2*n)+1)
 let comp_n_m n = tdb ((2*n)+2)
 let rec comp_kn_sub_ps k n = if n = 0 then [(comp_n_m k,true);(comp_n_r k,false);(comp_n_l k,false)] else (comp_n_m (k+n),true)::(comp_n_r (k+n),false)::(comp_kn_sub_ps k (n-1))
-let _comp_n_sub_ps n = comp_kn_sub_ps 0 n
 let comp_kn_tm k n = Coh(Builtin.comp_n n, comp_kn_sub_ps k (n-1))
 let comp_n_tm n = comp_kn_tm 0 n
 let comp_unary x y f = Coh(Builtin.comp_n 1, [(f,true);(y,false);(x,false)])
@@ -34,24 +41,26 @@ let rec sqt_comp_sub_ps n = if n = 0 then [(sqtm 0,true);(sqtr 0,false);(sqtl 0,
 let sqb_comp n = Coh(Builtin.comp_n n, sqb_comp_sub_ps (n-1))
 let sqt_comp n = Coh(Builtin.comp_n n, sqt_comp_sub_ps (n-1))
 let rec sqb_corner_comp_sub_ps n = if n = 0 then (sqb_sub_ps 0) else (sqbm n,true)::(sqbr n,false)::(sqb_corner_comp_sub_ps (n-1))
-let sqt_corner_comp_sub_ps n = (sqmr n,true)::(sqbr n,false)::(sqtm n,true)::(sqtr n,false)::(if n>0 then (sqt_comp_sub_ps (n-1)) else [])
+let sqt_corner_comp_sub_ps n = (sqmr n,true)::(sqbr n,false)::(sqt_comp_sub_ps n)
 
+(*
+https://q.uiver.app/#q=WzAsNCxbMCwwLCIwIl0sWzIsMCwiMyJdLFsyLDIsIjQiXSxbMCwyLCIxIl0sWzEsMiwiNSIsMV0sWzAsMywiMiIsMV0sWzEsMywiOCIsMSx7InNob3J0ZW4iOnsic291cmNlIjoyMCwidGFyZ2V0IjoyMH0sImxldmVsIjoyfV0sWzAsMSwiNiIsMV0sWzMsMiwiNyIsMV0sWzMsMiwiNyIsMSx7ImN1cnZlIjozfV0sWzAsMSwiNiIsMSx7ImN1cnZlIjotM31dLFs4LDksIiIsMSx7InNob3J0ZW4iOnsic291cmNlIjoyMCwidGFyZ2V0IjoyMH19XSxbMTAsNywiIiwxLHsic2hvcnRlbiI6eyJzb3VyY2UiOjIwLCJ0YXJnZXQiOjIwfX1dXQ==
+*)
 let ccomp_unary =
     let unbias = comp_n_tm 2 in
     let biasl = comp_binary (tdb 0) (tdb 1) (comp_unary (tdb 0) (tdb 1) (tdb 2)) (tdb 3) (tdb 4) in
     let biasr = comp_binary (tdb 0) (tdb 1) (tdb 2) (tdb 3) (comp_unary (tdb 1) (tdb 3) (tdb 4)) in
     (* Phase 1 *)
-    let phase1_sub_ps = sqb_sub_ps 0 in
-    let phase1_sub = Unchecked.list_to_db_level_sub (List.map fst phase1_sub_ps) in
-    let phase1 = Coh(Coh.check_inv (Builtin.ps_comp 2) biasr unbias ("builtin_unbiasor",0,None),phase1_sub_ps) in
-    let phase1_sub_contr = [(phase1,true);(Unchecked.tm_apply_sub unbias phase1_sub,false);(Unchecked.tm_apply_sub biasr phase1_sub,false);(sqbr 0,false);(sqtl 0,false)] in
-    (* Phase 3 *)
-    let phase3_sub_ps = sqt_sub_ps 0 in
-    let phase3_sub = Unchecked.list_to_db_level_sub (List.map fst phase3_sub_ps) in
-    let phase3 = Coh(Coh.check_inv (Builtin.ps_comp 2) unbias biasl ("builtin_biasor",0,None),phase3_sub_ps) in
-    let phase3_sub_contr = [(phase3,true);(Unchecked.tm_apply_sub biasl phase3_sub,false)] in
+    let phase1_sub_ps = sqt_sub_ps 0 in
+    let phase1 = Coh(Coh.check_inv (Builtin.ps_comp 2) biasl unbias ("builtin_unbiasl",0,None),phase1_sub_ps) in
+    let phase1_sub_contr = Unchecked.coh_to_sub_ps phase1 in
     (* Phase 2 *)
-    let phase2_sub_contr = [(tdb 8,true);(Unchecked.tm_apply_sub unbias phase3_sub,false)] in
+    let phase2_sub_contr = [(sqmm 0,true);(sqb 0,false)] in
+    (* Phase 3 *)
+    let phase3_sub_ps = sqb_sub_ps 0 in
+    let phase3_sub,_ = Unchecked.sub_ps_to_sub phase3_sub_ps (Builtin.ps_comp 2) in
+    let phase3 = Coh(Coh.check_inv (Builtin.ps_comp 2) unbias biasr ("builtin_biasr",0,None),phase3_sub_ps) in
+    let phase3_sub_contr = [(phase3,true);(Unchecked.tm_apply_sub biasr phase3_sub,false)] in
     (* Collate *)
     let comp_sub = List.concat [phase3_sub_contr;phase2_sub_contr;phase1_sub_contr] in
     let comp = Suspension.coh (Some(1)) (Builtin.comp_n 3) in
@@ -65,65 +74,63 @@ let ccomp_binary =
     let assocr = comp_binary (tdb 0) (tdb 1) (tdb 2) (tdb 5) (comp_binary (tdb 1) (tdb 3) (tdb 4) (tdb 5) (tdb 6)) in
     let assocl = comp_binary (tdb 0) (tdb 3) (comp_n_tm 2) (tdb 5) (tdb 6) in
     (* Phase 1 *)
-    let phase1_sub_ps = List.concat [[(sqbm 1,true);(sqbr 1,false)];(sqb_sub_ps 0)] in
-    let phase1_sub = Unchecked.list_to_db_level_sub (List.map fst phase1_sub_ps) in
-    let phase1 = Coh(Coh.check_inv (Builtin.ps_comp 3) assocr assocl ("builtin_assoc",0,None),phase1_sub_ps) in
-    let phase1_sub_contr = [(phase1,true);(Unchecked.tm_apply_sub assocl phase1_sub,false);(Unchecked.tm_apply_sub assocr phase1_sub,false);(sqbr 1,false);(sqtl 0,false)] in
+    let phase1_sub_ps = sqt_corner_comp_sub_ps 1 in
+    let phase1 = Coh(Coh.check_inv (Builtin.ps_comp 3) assocl assocr ("builtin_assoc",0,None),phase1_sub_ps) in
+    let phase1_sub_contr = Unchecked.coh_to_sub_ps phase1 in
     (* Phase 2 *)
-    let phase2_sub_ps = [(sqbm 1,true);(sqbr 1,false);(sqmm 0,true);(sqt 0,false);(sqb 0,false);(sqbr 0,false);(sqtl 0,false)] in
-    let phase2 = Coh(Functorialisation.whisk 0 1 0,phase2_sub_ps) in
-    let phase2_sub_contr = [(phase2,true);(comp_binary (sqtl 0) (sqbr 0) (sqt 0) (sqbr 1) (sqbm 1),false)] in
+    let phase2_sub_ps = [(sqmm 1,true);(sqb 1,false);(sqt 1,false);(sqbr 1,false);(sqtm 0,true);(sqtr 0,false);(sqtl 0,false)] in
+    let phase2 = Coh(Functorialisation.whisk 0 0 1, phase2_sub_ps) in
+    let phase2_sub_contr = [(phase2,true);(comp_binary (sqtl 0) (sqtr 0) (sqtm 0) (sqbr 1) (sqb 1),false)] in
     (* Phase 3 *)
     let phase3_sub_ps = List.concat [[(sqbm 1,true);(sqbr 1,false)];(sqt_sub_ps 0)] in
     let phase3_sub = Unchecked.list_to_db_level_sub (List.map fst phase3_sub_ps) in
-    let phase3 = Coh(Coh.check_inv (Builtin.ps_comp 3) assocl assocr ("builtin_assoc_inv",0,None),phase3_sub_ps) in
-    let phase3_sub_contr = [(phase3,true);(Unchecked.tm_apply_sub assocr phase3_sub,false)] in
+    let phase3 = Coh(Coh.check_inv (Builtin.ps_comp 3) assocr assocl ("builtin_assoc_inv",0,None),phase3_sub_ps) in
+    let phase3_sub_contr = [(phase3,true);(Unchecked.tm_apply_sub assocl phase3_sub,false)] in
     (* Phase 4 *)
-    let phase4_sub_ps = [(sqmm 1,true);(sqt 1,false);(sqb 1,false);(sqbr 1,false);(sqtm 0,true);(sqtr 0,false);(sqtl 0,false)] in
-    let phase4 = Coh(Functorialisation.whisk 0 0 1, phase4_sub_ps) in
-    let phase4_sub_contr = [(phase4,true);(comp_binary (sqtl 0) (sqtr 0) (sqtm 0) (sqbr 1) (sqt 1),false)] in
+    let phase4_sub_ps = [(sqbm 1,true);(sqbr 1,false);(sqmm 0,true);(sqb 0,false);(sqt 0,false);(sqbr 0,false);(sqtl 0,false)] in
+    let phase4 = Coh(Functorialisation.whisk 0 1 0,phase4_sub_ps) in
+    let phase4_sub_contr = [(phase4,true);(comp_binary (sqtl 0) (sqbr 0) (sqb 0) (sqbr 1) (sqbm 1),false)] in
     (* Phase 5 *)
-    let phase5_sub_ps = [(sqmr 1,true);(sqbr 1,false);(sqtm 1,true);(sqtr 1,false);(sqtm 0,true);(sqtr 0,false);(sqtl 0,false)] in
+    let phase5_sub_ps = List.concat [[(sqbm 1,true);(sqbr 1,false)];(sqb_sub_ps 0)] in
     let phase5_sub = Unchecked.list_to_db_level_sub (List.map fst phase5_sub_ps) in
-    let phase5 = Coh(Coh.check_inv (Builtin.ps_comp 3) assocr assocl ("builtin_assoc",0,None),phase5_sub_ps) in
-    let phase5_sub_contr = [(phase5,true);(Unchecked.tm_apply_sub assocl phase5_sub,false);] in
-    (* Merge *)
+    let phase5 = Coh(Coh.check_inv (Builtin.ps_comp 3) assocl assocr ("builtin_assoc",0,None),phase5_sub_ps) in
+    let phase5_sub_contr = [(phase5,true);(Unchecked.tm_apply_sub assocr phase5_sub,false)] in
+    (* Collate *)
     let comp_sub = List.concat [phase5_sub_contr;phase4_sub_contr;phase3_sub_contr;phase2_sub_contr;phase1_sub_contr] in
     let comp = Suspension.coh (Some(1)) (Builtin.comp_n 5) in
     Coh(comp,comp_sub)
 
-let rec ccomp_n arity =
+let rec build_ccomp_n arity =
     match arity with
     | 1 -> ccomp_unary
     | 2 -> ccomp_binary
     | _ -> begin
+      (* Compute inductive case *)
       let sq_ind = ccomp_n (arity-1) in
+      (* Various biased composites *)
       let ps = Builtin.ps_comp (arity+1) in
       let unbiasl = comp_binary (comp_n_l 0) (comp_n_r (arity-1)) (comp_n_tm arity) (comp_n_r arity) (comp_n_m arity) in
       let unbiasr = comp_binary (comp_n_l 0) (comp_n_r 0) (comp_n_m 0) (comp_n_r arity) (comp_kn_tm 1 arity) in
       let biasl = comp_binary (comp_n_l 0) (comp_n_r (arity-1)) (comp_binary (comp_n_l 0) (comp_n_r (arity-2)) (comp_n_tm (arity-1)) (comp_n_r (arity-1)) (comp_n_m (arity-1))) (comp_n_r arity) (comp_n_m arity) in
       let biasr = comp_binary (comp_n_l 0) (comp_n_r 0) (comp_n_m 0) (comp_n_r arity) (comp_binary (comp_n_l 1) (comp_n_r (arity-1)) (comp_kn_tm 1 (arity-1)) (comp_n_r arity) (comp_n_m arity)) in
       let sqb_corner_sub_ps = sqb_corner_comp_sub_ps (arity-1) in
-      let sqt_corner_sub_ps = sqt_corner_comp_sub_ps (arity-1) in
       let sqb_corner_sub = fst (Unchecked.sub_ps_to_sub sqb_corner_sub_ps ps) in
-      let sqt_corner_sub = fst (Unchecked.sub_ps_to_sub sqt_corner_sub_ps ps) in
       (* Phase 1 *)
-      let phase1 = Coh(Coh.check_inv ps unbiasr biasr ("builtin_bias",0,None), sqb_corner_sub_ps) in
-      let phase1_src = Unchecked.tm_apply_sub unbiasr sqb_corner_sub in
-      let phase1_tgt = Unchecked.tm_apply_sub biasr sqb_corner_sub in
+      let phase1 = Coh(Coh.check_inv ps unbiasl biasl ("builtin_bias_left",0,None), sqt_corner_comp_sub_ps (arity-1)) in
       (* Phase 2 *)
       let phase2_presub = [sqmm (arity-1);sqbm (arity-1);sqtm (arity-1);sqmr (arity-1);sqbr (arity-1);sqtr (arity-1);sq_ind;sqb_comp (arity-1);sqt_comp (arity-1);sqml (arity-1);sqbl (arity-1);sqtl (arity-1);sqml 0;sqbl 0;sqtl 0] in
       let phase2_sub = Unchecked.list_to_db_level_sub phase2_presub in
       let phase2 = Unchecked.tm_apply_sub ccomp_binary phase2_sub in
-      let phase2_tgt = Unchecked.tm_apply_sub biasl sqt_corner_sub in
+      let phase2_tgt = Unchecked.tm_apply_sub biasr sqb_corner_sub in
       (* Phase 3 *)
-      let phase3 = Coh(Coh.check_inv ps biasl unbiasl ("builtin_bias",0,None), sqt_corner_sub_ps) in
-      let phase3_tgt = Unchecked.tm_apply_sub unbiasl sqt_corner_sub in
+      let phase3 = Coh(Coh.check_inv ps biasr unbiasr ("builtin_bias_right",0,None), sqb_corner_sub_ps) in
+      let phase3_tgt = Unchecked.tm_apply_sub unbiasr sqb_corner_sub in
       (* Merge *)
-      let comp_sub = [(phase3,true);(phase3_tgt,false);(phase2,true);(phase2_tgt,false);(phase1,true);(phase1_tgt,false);(phase1_src,false);(sqbr (arity-1),false);(sqtl 0,false)] in
-      let _ = Unchecked.sub_ps_to_sub comp_sub (Unchecked.suspend_ps (Builtin.ps_comp 3)) in
+      let comp_sub = List.concat [[(phase3,true);(phase3_tgt,false);(phase2,true);(phase2_tgt,false)];(Unchecked.coh_to_sub_ps phase1)] in
       Coh(Suspension.coh (Some(1)) (Builtin.comp_n 3), comp_sub)
     end
+and ccomp_n arity =
+  Memo.find_ccomp arity build_ccomp_n
 
 (* Interchange needed for source of depth-1 non-inv coh *)
 (*
@@ -251,8 +258,8 @@ let rec depth1_bridge_sub src_sub tgt_sub l ctxf =
         let linear_ps,_,_ = Coh.forget (Builtin.comp_n arity) in
         let linear_ctx = Unchecked.ps_to_ctx linear_ps in
         let linear_ctxf = Functorialisation.ctx linear_ctx (List.map fst linear_ctx) in
-        let _ = check_term (Ctx.check linear_ctxf) (Unchecked.tm_apply_sub (Opposite.tm (ccomp_n arity) [2]) (Unchecked.db_level_sub linear_ctxf)) in
-        let ccomp = Suspension.tm (Some(d-1)) (Opposite.tm (ccomp_n arity) [2]) in
+        let _ = check_term (Ctx.check linear_ctxf) (Unchecked.tm_apply_sub (ccomp_n arity) (Unchecked.db_level_sub linear_ctxf)) in
+        let ccomp = Suspension.tm (Some(d-1)) (ccomp_n arity) in
         let inner_subf = Functorialisation.sub inner_sub l ctxf in
         let inner_subf_norm = Unchecked.list_to_db_level_sub (List.map fst inner_subf) in
         let bridge = Unchecked.tm_apply_sub ccomp inner_subf_norm in
