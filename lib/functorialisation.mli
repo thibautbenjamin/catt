4cd946fe45118a393d431c7e9f8f5bd836750118
regular-citizen <chiara@sarti.me>
2024-06-06 17:35:06 +0100

    added comments and removed checking code

--- lib/functorialisation.mli
+++ lib/functorialisation.mli
@@ -3,19 +3,19 @@ open Kernel
 open Unchecked_types.Unchecked_types(Coh)
 
 val cubical_comp : (int -> tm) ref
-val depth1_interchanger_src : (Coh.t -> (Var.t list) -> ctx -> tm * ty) ref
-val depth1_interchanger_tgt : (Coh.t -> (Var.t list) -> ctx -> tm * ty) ref
-val depth1_bridge_sub : (sub_ps -> sub_ps -> (Var.t list) -> ctx -> sub_ps) ref
+val depth1_interchanger_src : (Coh.t -> (Var.t list) -> tm * ty) ref
+val depth1_interchanger_tgt : (Coh.t -> (Var.t list) -> tm * ty) ref
+val depth1_bridge_sub : (sub_ps -> sub_ps -> (Var.t list) -> sub_ps) ref
 
 val find_places : ctx -> sub_ps -> (Var.t list) -> (Var.t list)
 val tgt_subst : (Var.t list) -> sub
 
 val coh : Coh.t -> functorialisation_data -> Coh.t
 val coh_all : Coh.t -> Coh.t
-val tm_one_step_tm : tm -> (Var.t list) -> ctx -> tm
-val ty : ty -> (Var.t list) -> tm -> ctx -> ty
+val tm_one_step_tm : tm -> (Var.t list) -> tm
+val ty : ty -> (Var.t list) -> tm -> ty
 val ctx : ctx -> (Var.t list) -> ctx
-val sub : sub_ps -> (Var.t list) -> ctx -> sub_ps
+val sub : sub_ps -> (Var.t list) -> sub_ps
 val tm : ctx -> tm -> functorialisation_data -> ctx * tm
 
 val whisk : int -> int -> int -> Coh.t
