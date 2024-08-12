0346118ededb40fa95fb5368769e94d0d1733fb6
regular-citizen <chiara@sarti.me>
2024-08-12 21:49:16 +0100

    added phase structure infra for cones and implemented phases for 101

--- lib/cones.mli
+++ lib/cones.mli
@@ -2,9 +2,9 @@ open Common
 open Kernel
 open Unchecked_types.Unchecked_types(Coh)
 
-val cone_ty : tm -> ty -> (Var.t*Var.t) list -> tm -> ty
-val cone_tm : tm -> (Var.t*Var.t) list -> tm -> tm
+val cone_ty : tm -> ty -> (Var.t*Var.t) list -> Var.t -> ty
+val cone_tm : tm -> (Var.t*Var.t) list -> Var.t -> tm
 val cone_ctx : ctx -> ctx * ((Var.t*Var.t) list * Var.t)
-val cone_sub : sub -> (Var.t*Var.t) list -> Var.t -> Var.t -> sub
+val cone_sub : sub -> (Var.t*Var.t) list -> (Var.t*Var.t) list -> Var.t -> Var.t -> sub
 
 val cones_postprocess_fn : ctx -> tm -> ctx*tm
