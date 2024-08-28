b4026b212225368dbb70b4591f003191b64fc82e
regular-citizen <chiara@sarti.me>
2024-08-28 11:10:06 +0100

    kernel performance improvement

--- lib/kernel.ml
+++ lib/kernel.ml
@@ -110,7 +110,7 @@ and Ctx
   let value ctx = ctx.c
   let forget c = c.unchecked
   let to_string ctx = Unchecked.ctx_to_string (forget ctx)
-  let check_equal ctx1 ctx2 =
+  let check_equal ctx1 ctx2 = if ctx1 == ctx2 then () else
     Unchecked.check_equal_ctx (forget ctx1) (forget ctx2)
 
   let check_notin ctx x =
@@ -273,7 +273,7 @@ struct
   let target ps =
     Sub.check_to_ps (to_ctx ps) (Unchecked.ps_tgt ps.tree) (bdry ps)
 
-  let check_equal ps1 ps2 =
+  let check_equal ps1 ps2 = if ps1.tree == ps2.tree then () else
     Unchecked.check_equal_ps ps1.tree ps2.tree
 end
 
