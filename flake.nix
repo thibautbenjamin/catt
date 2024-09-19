7a2f7d223620a38bde8624e6b0a2c1c02ffca994
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2024-09-19 15:02:02 +0100

    run dune tests with `nix flake check`

--- flake.nix
+++ flake.nix
@@ -5,12 +5,33 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
+    nix-filter.url = "github:numtide/nix-filter";
   };
 
-  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, nix-filter,... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = (import nixpkgs { inherit system; });
           ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_14;
+          sources = {
+            ocaml = nix-filter.lib {
+              root = ./.;
+              include = [
+                ".ocamlformat"
+                "dune-project"
+                (nix-filter.lib.inDirectory "bin")
+                (nix-filter.lib.inDirectory "lib")
+                (nix-filter.lib.inDirectory "test.t")
+              ];
+            };
+
+            nix = nix-filter.lib {
+              root = ./.;
+              include = [
+                (nix-filter.lib.matchExt "nix")
+              ];
+            };
+          };
+
       in {
         packages = {
           default = self.packages.${system}.catt;
@@ -19,8 +40,9 @@
             pname = "catt";
             version = "1.0";
             minimalOcamlVersion = "4.08";
+            doCheck = false;
 
-            src = ./.;
+            src = sources.ocaml;
 
             nativeBuildInputs = with ocamlPackages;
               [ menhir
@@ -49,11 +71,18 @@
           };
         };
 
+        checks = {
+          default = self.packages.${system}.catt.overrideAttrs
+            (oldAttrs: {
+              name = "check-${oldAttrs.name}";
+              dontInstall = true;
+              doCheck = true;
+            });
+        };
+
         devShells.default = pkgs.mkShell {
           packages =
-            (with pkgs;
-              [  nixpkgs-fmt
-                 fswatch ])
+            (with pkgs; [  nixpkgs-fmt fswatch ])
             ++
             (with ocamlPackages;
               [ odoc
