d56af143bb014f69c75aca298e5530f835873bb0
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2024-09-24 11:26:45 +0100

    [coq-plugin] architecture

---
+++ coq_plugin/src/export.ml
@@ -0,0 +1,5 @@
+open Catt
+
+let example () =
+  Io.debug "hello";
+  ()
---
+++ coq_plugin/theories/Test.v
@@ -0,0 +1,9 @@
+From Catt Require Import Loader.
+
+(* Print Registered. *)
+
+(* CaTT. *)
+
+(* Print Registered. *)
+
+(* Check catt_comp. *)
