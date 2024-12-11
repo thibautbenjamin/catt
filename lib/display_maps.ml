a1dfe0c63f114ab6ea919ec88d6169c4b9b47875
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2024-12-11 21:28:18 +0000

    universal property of pullback along display maps

--- lib/display_maps.ml
+++ lib/display_maps.ml
@@ -15,4 +15,28 @@ let rec pullback c1 sub c2 dm =
       let newvar = Var.fresh () in
       let ty = Unchecked.ty_apply_sub ty names in
       ((newvar, (ty, expl)) :: ctx, (x, (Var newvar, expl)) :: names)
-  | [], _ :: _ -> assert false
+  | [], _ :: _ ->
+      Error.fatal
+        "wrong data for pullback: display map cannot be longer than the context"
+
+let rec glue s1 s2 right_incl c2 dm =
+  match (s2, c2, dm) with
+  | [], [], [] -> s1
+  | (z, _) :: s2, (x, _) :: c2, (_, (Var y, _)) :: dm when x = y && x = z ->
+      let s = glue s1 s2 right_incl c2 dm in
+      s
+  | (z, (t, e)) :: s2, (x, _) :: c2, (_ as dm) when x = z ->
+      let s = glue s1 s2 right_incl c2 dm in
+      let var =
+        match Unchecked.tm_apply_sub (Var x) right_incl with
+        | Var x -> x
+        | _ -> assert false
+      in
+      (var, (t, e)) :: s
+  | _, [], _ :: _ ->
+      Error.fatal
+        "wrong data for pullback gluing: display map cannot be longer than the \
+         context"
+  | _, _, _ ->
+      Error.fatal
+        "wrong data pullback gluing: substitution must be point to the context"
