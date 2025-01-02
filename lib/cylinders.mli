bf16ab282d120c006c69b2aa7e3d75abcedc405b
Thibaut Benjamin <thibaut.benjamin@gmail.com>
2025-01-02 18:20:39 +0000

    [cylinders] cylinder stacking

--- lib/cylinders.mli
+++ lib/cylinders.mli
@@ -1,12 +1,4 @@
 open Kernel
-open Unchecked_types.Unchecked_types(Coh)(Tm)
-
-module Cylinder : sig
-  val ctx : int -> ctx
-end
-
-module Codim1 : sig
-  val ctx : int -> ctx
-end
 
 val compose : int -> int -> int -> Tm.t
+val stacking : int -> Tm.t
