089ccff6a42e9da044094e0e93475de529fd6a4e
Wilf Offord <111688351+wilfofford@users.noreply.github.com>
2025-08-15 10:49:49 +0100

    [constructs] improved api and facilities

--- lib/construct.mli
+++ lib/construct.mli
@@ -2,11 +2,40 @@ open Common
 open Kernel
 open Unchecked_types.Unchecked_types(Coh)(Tm)
 
-type constr = tm * ty
-
+val to_tm : constr -> tm
+val to_ty : constr -> ty
+val arr : constr -> constr -> ty
+val characteristic_sub_ps : constr -> sub_ps
+val coh_app : Coh.t -> constr list -> constr
+val of_coh : Coh.t -> constr
+val make_sub : ctx -> constr list -> sub
+val tm_app_sub : Tm.t -> sub -> constr
+val tm_app : Tm.t -> constr list -> constr
+val of_tm : Tm.t -> constr
 val wcomp : constr -> int -> constr -> constr
 val wcomp3 : constr -> int -> constr -> int -> constr -> constr
 val intch_comp_nm : constr -> constr -> constr -> constr
 val intch_comp_mn : constr -> constr -> constr -> constr
 val opposite : constr -> op_data -> constr
 val inv : constr -> constr
+val functorialise : constr -> Var.t list -> constr
+val id : constr -> constr
+val id_n : int -> constr -> constr
+val dim : constr -> int
+val apply_sub : constr -> sub -> constr
+val apply_sub_ps : constr -> sub_ps -> constr
+val rename : constr -> (Var.t * tm) list -> constr
+val bdry : int -> constr -> constr * constr
+val src : int -> constr -> constr
+val tgt : int -> constr -> constr
+val inverse : constr -> constr
+val suspend : int -> constr -> constr
+val comp_n : constr list -> constr
+val comp : constr -> constr -> constr
+val comp3 : constr -> constr -> constr -> constr
+val op : int list -> constr -> constr
+val witness : constr -> constr
+val glue_subs_along : int -> 'a list list -> 'a list
+val wcomp_n : int -> constr list -> constr
+val characteristic_sub_ps_composite : constr list -> sub_ps
+val develop : constr -> constr
